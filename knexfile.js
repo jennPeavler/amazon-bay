7d4130bb7de89b917581cb29992d64b522d420c4
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-27 09:55:53 -0600

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/amazon_bay_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/amazon_bay',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+     directory: './db/seeds/dev'
+   }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const router = require('./router');
+const bodyParser = require('body-parser');
+
+const port = (process.env.PORT || 3000);
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use('/assets', express.static(path.join(__dirname, '../client/assets/')));
+
+app.use('/api/v1', router);
+
+app.listen(port, () => {
+  console.log(`amazon-bay server listening on port ${port}!`);
+});
+
+module.exports = app;
