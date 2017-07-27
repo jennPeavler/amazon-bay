da16824b34100d713310336ddf4af517bdc6faa4
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-27 16:41:08 -0600

    add production env to knexfile

--- knexfile.js
+++ knexfile.js
@@ -20,5 +20,17 @@ module.exports = {
     seeds: {
      directory: './db/seeds/dev'
    }
+  },
+
+ production: {
+   client: 'pg',
+   connection: `${process.env.DATABASE_URL}?ssl=true`,
+   migrations: {
+     directory: './db/migrations',
+   },
+   useNullAsDefault: true,
+   seeds: {
+     directory: './db/seeds/dev',
+   },
   }
 };
